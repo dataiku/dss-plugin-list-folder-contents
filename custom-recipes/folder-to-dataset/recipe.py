06c5d89304603a642e9848a29d1362932250bde6
jplassmann <jeremy.plassmann@gmail.com>
2021-09-22 10:13:49 +0100

    [sc-71089] add support to extension depth ...

--- custom-recipes/folder-to-dataset/recipe.py
+++ custom-recipes/folder-to-dataset/recipe.py
@@ -13,13 +13,11 @@ from dku_io_utils import generate_path_list
 input_folder = dataiku.Folder(get_input_names_for_role("input_folder")[0])
 output_dataset = dataiku.Dataset(get_output_names_for_role("output_dataset")[0])
 level_mapping_string = get_recipe_config().get('level_mapping')
-print("LEVEL MAPPING")
-print(level_mapping_string)
 
 mapping = get_level_mapping(level_mapping_string)
 
-path_list = generate_path_list(input_folder)
+path_details_list = generate_path_list(input_folder)
 
-list_row_path = pd.DataFrame([compute_columns_from_path(path, mapping) for path in path_list])
+list_row_path = pd.DataFrame([compute_columns_from_path(path_detail, mapping) for path_detail in path_details_list])
 
 output_dataset.write_with_schema(list_row_path)
